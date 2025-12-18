d761cf9642c072f149a7b073e22d5dd80e65d99b
cloudloyola15 <cloudloyola15@gmail.com>
2025-12-18 22:19:22 +0530

    Ready for deployment

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, File, UploadFile, HTTPException
-import boto3
-import os
+import boto3, os
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# 🔴 CHANGE THIS TO YOUR REAL BUCKET NAME
-S3_BUCKET = "my-fastapi-upload-bucket"
 
+S3_BUCKET = "your-bucket-name"
 AWS_REGION = os.getenv("AWS_DEFAULT_REGION")
 
 s3 = boto3.client(
@@ -24,28 +21,7 @@ s3 = boto3.client(
     region_name=AWS_REGION,
 )
 
-ALLOWED_EXTENSIONS = {".csv", ".xls", ".xlsx"}
-
-
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    ext = os.path.splitext(file.filename)[1].lower()
-
-    if ext not in ALLOWED_EXTENSIONS:
-        raise HTTPException(status_code=400, detail="Invalid file type")
-
-    try:
-        s3.upload_fileobj(
-            file.file,
-            S3_BUCKET,
-            file.filename,
-            ExtraArgs={"ContentType": file.content_type},
-        )
-    except Exception as e:
-        print("🔥 S3 ERROR:", repr(e))
-        raise HTTPException(status_code=500, detail=str(e))
-
-    return {
-        "filename": file.filename,
-        "message": "Upload successful",
-    }
+    s3.upload_fileobj(file.file, S3_BUCKET, file.filename)
+    return {"message": "Upload successful"}
